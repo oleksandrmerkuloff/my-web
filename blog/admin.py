2f0a81022e9a84a24558929ba7bf2bcb013e38e8
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-03-12 17:51:25 +0200

    setup admin for post

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import Post, Tag, PostImages
 
 
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'outline', 'body', 'tags']
+    list_display = ['title', 'created_at']
+    list_display_links = ['title',]
+    list_filter = ['created_at',]
+
+
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostImages)
